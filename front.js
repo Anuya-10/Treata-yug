8868ff91d98dc4d9ca396ec31c3aee8b77a654c6
Your Name <your-email@example.com>
2025-02-06 16:17:02 +0530

    Added front.js

---
+++ front.js
@@ -0,0 +1,35 @@
+const images=document.querySelectorAll('.image-gallery img');
+let currentIndex=0;
+
+function display(){
+    images[currentIndex].classList.remove('active');
+    currentIndex=(currentIndex+1)%images.length;
+    images[currentIndex].classList.add('active'); 
+    console.log("hi"); 
+}
+
+setInterval(display,2000);
+document.querySelector('.two').addEventListener('click',function(){
+    window.location.href='episodes.html';
+    
+})
+document.querySelector('.six').addEventListener('click',function(){
+    window.location.href='learnpage.html';
+    
+})
+document.querySelector('.one').addEventListener('click',function(){
+    window.location.href='sl.html';
+    
+})
+document.querySelector('.three').addEventListener('click',function(){
+    window.location.href='gamepage.html';
+    
+})
+document.querySelector('.four').addEventListener('click',function(){
+    window.location.href='blog.html';
+    
+})
+document.querySelector('.five').addEventListener('click',function(){
+    window.location.href='bhajans.html';
+    
+})
